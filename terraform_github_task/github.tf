a1cd93a6b08262eac8fcadbfe7718e0b05ea1da9
stinkytroll <61510983+stinkytroll@users.noreply.github.com>
2024-10-23 16:20:03 +0100

    Minor changes

--- terraform_github_task/github.tf
+++ terraform_github_task/github.tf
@@ -1,18 +1,18 @@
 # GitHub Provider setup
 
 provider "github" {
-      # This uses the token from the variable
+  token = var.github_token
 }
 
 # Create a new GitHub repository
-resource "github_repository" "tech2xx_repo" {
+resource "github_repository" "tech264_repo" {
   name        = "tech264-tf-create-github-repo"
   description = "A new repo created using Terraform"
-  visibility  = "public"  # Set to "private" if needed
-  auto_init   = true      # Automatically create an initial commit with a README
+  visibility  = "public" # Set to "private" if needed
+  auto_init   = true     # Automatically create an initial commit with a README
 }
 
 # Outputs to verify repository details
 output "repo_url" {
-  value = github_repository.tech2xx_repo.html_url
+  value = github_repository.tech264_repo.html_url
 }
